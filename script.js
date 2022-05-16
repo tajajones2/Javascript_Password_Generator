a2623c32ff01c0365e65eb7cc38ae5373135bd7d
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-05-16 18:42:38 -0400

    added function for generate password

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-// Assignment Code
+// 
+//  1. define character arrays (ex. lower case, uppercase, special, password, user choices)
+//  2  get user's password length 
+//  3. define function to read inputs from user 
+//  4. define 
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+  function generatePassword (){
+    var size = prompt("please enter 8 to 12 characters")
+    console.log(size)
 
 }
 
