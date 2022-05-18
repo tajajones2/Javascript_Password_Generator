55ab1ebe8a08f99b1511248d59f02c3998987c52
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-05-17 22:04:12 -0400

    if statement for loop for pasword length

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //  1. Prompt the user for password criteria 
-      // a.) password length must be btween 8-128 characters 
-      // b.) lowercase, uppercase, numbers, special characters, 
+// a.) password length must be btween 8-128 characters 
+// b.) lowercase, uppercase, numbers, special characters, 
 
 //  2.) Validate the input
 //  3.) Generate password based on criteria.
 
 //  4.) And then Display the password on the page 
- 
 
 
-var lowercase = "abcdefghijklmnopqrstuvwx";
+
+var aplha = "abcdefghijklmnopqrstuvwx";
 var number = "0123456789";
 var special = "!@#$%^&*()";
 var finalpwd = "";
@@ -22,67 +22,84 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
 
   passwordText.value = password;
 }
 
-  function generatePassword (){
-    var size = prompt("Please enter password length (between 8-128 charcters)");
-    console.log(size);
-  
-// validation for checking if true/false
-    if(size>= 8 && size <=128) {
-      var isLowercase = confirm("include lowercase?");
-      if (isLowercase === true) {
-        charType.push("lower")
+function generatePassword() {
+  var size = prompt("Please enter password length (between 8-128 charcters)");
+  console.log(size);
+
+  // validation for checking if true/false
+  if (size >= 8 && size <= 128) {
+    var isLowercase = confirm("include lowercase?");
+    if (isLowercase === true) {
+      charType.push("lower")
       console.log(isLowercase);
-      }
+    }
 
-      var isUppercase = confirm("include Uppercase?");
-      if (isUppercase === true) {
-        charType.push("upper")
+    var isUppercase = confirm("include Uppercase?");
+    if (isUppercase === true) {
+      charType.push("upper")
       console.log(isUppercase);
-      }
+    }
 
-      var isNumber = confirm("include Number?");
-      if (isNumber === true) {
-        charType.push("number")
+    var isNumber = confirm("include Number?");
+    if (isNumber === true) {
+      charType.push("number")
       console.log(isNumber);
-      }
+    }
 
-      var isSpecial = confirm("include Special?");
-      if (isSpecial === true) {
-        charType.push("special")
+    var isSpecial = confirm("include Special?");
+    if (isSpecial === true) {
+      charType.push("special")
       console.log(isSpecial);
-      }
-
-      // if (isLowercase ===true) {
-      //   var positionString=Math.floor(Math.random() *26);
-      
-      
-        finalpwd=finalpwd + lowercase[positionString];
-        // console.log(finalpwd);
-        return finalpwd;
-
-// for loop for pasword length 
-       for (i = 0; i < size; i++) {
-         var randomText = Math.floor(Math.random() *size
-         
-       }
-
-      }
-      
-    }
-    else {
-      alert('invalid entery')
     }
 
+    // if (isLowercase ===true) {
+    //   var positionString=Math.floor(Math.random() *26);
+
+
+    finalpwd = finalpwd + lowercase[positionString];
+    // console.log(finalpwd);
     
 
+    // for loop for pasword length 
+    for (i = 0; i < size; i++) {
+      var randomType = Math.floor(Math.random() * charType.length)
+      
+      if (randomType === "lower") {
+        var randomLower = math.floor(Math.random() *26);
+        finalpwd=finalpwd + aplha[randomLower]
+      }
 
+      if (randomType === "upper") {
+        var randomUpper= math.floor(Math.random() *26);
+        finalpwd=finalpwd + aplha[randomUpper].toUpperCase();
+      }
 
-} 
+      if (randomType === "number") {
+        var randomNumber= math.floor(Math.random() *9);
+        finalpwd=finalpwd + number[randomNumber]
+
+      }
+
+      if (randomType === "special") {
+        var randomSpecial = math.floor(Math.random() *special.length);
+        finalpwd=finalpwd + special[randomSpecial]
+
+      }
+    }
+
+  } else {
+  alert('invalid entry')
+  }
+
+  return finalpwd;
+}
+
+    
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
