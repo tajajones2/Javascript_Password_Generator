e78b0ee25f07d9f06b84532e5e71fdcff2de02ea
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-05-17 21:22:34 -0400

    added if statements for true or false

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var lowercase = "abcdefghijklmnopqrstuvwx";
 var number = "0123456789";
 var special = "!@#$%^&*()";
 var finalpwd = "";
+var charType = [];
 
 var generateBtn = document.querySelector("#generate");
 
@@ -29,30 +30,44 @@ function writePassword() {
   function generatePassword (){
     var size = prompt("Please enter password length (between 8-128 charcters)");
     console.log(size);
-    //then do validation, if im checking 
-
+  
+// validation for checking if true/false
     if(size>= 8 && size <=128) {
       var isLowercase = confirm("include lowercase?");
+      if (isLowercase === true) {
+        charType.push("lower")
       console.log(isLowercase);
+      }
 
       var isUppercase = confirm("include Uppercase?");
+      if (isUppercase === true) {
+        charType.push("upper")
       console.log(isUppercase);
+      }
 
       var isNumber = confirm("include Number?");
+      if (isNumber === true) {
+        charType.push("number")
       console.log(isNumber);
+      }
 
       var isSpecial = confirm("include Special?");
+      if (isSpecial === true) {
+        charType.push("special")
       console.log(isSpecial);
+      }
 
-      if (isLowercase ===true) {
-        var positionString=Math.floor(Math.random() *26);
+      // if (isLowercase ===true) {
+      //   var positionString=Math.floor(Math.random() *26);
       
       
         finalpwd=finalpwd + lowercase[positionString];
         // console.log(finalpwd);
         return finalpwd;
 
-       for (i=0; i < size; i++){
+// for loop for pasword length 
+       for (i = 0; i < size; i++) {
+         var randomText = Math.floor(Math.random() *size
          
        }
 
