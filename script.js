9d26e9b4951d8c644fc4aa60318d6e6bdc64679c
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-05-17 20:34:47 -0400

    added for loop and varibles to global and final paswword

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// 
-//  1. define character arrays (ex. lower case, uppercase, special, password, user choices)
-//  2  get user's password length 
-//  3. define function to read inputs from user 
-//  4. define 
+//  1. Prompt the user for password criteria 
+      // a.) password length must be btween 8-128 characters 
+      // b.) lowercase, uppercase, numbers, special characters, 
+
+//  2.) Validate the input
+//  3.) Generate password based on criteria.
+
+//  4.) And then Display the password on the page 
+ 
 
 
-var lowercase = "abcdefghijklmnopqrstuvwx"
-var number = "0123456789"
-var special = "!@#$%^&*()"
+var lowercase = "abcdefghijklmnopqrstuvwx";
+var number = "0123456789";
+var special = "!@#$%^&*()";
+var finalpwd = "";
 
 var generateBtn = document.querySelector("#generate");
 
@@ -22,24 +27,34 @@ function writePassword() {
 }
 
   function generatePassword (){
-    var size = prompt("Please enter 8 to 12 characters")
-    console.log(size)
+    var size = prompt("Please enter password length (between 8-128 charcters)");
+    console.log(size);
     //then do validation, if im checking 
 
     if(size>= 8 && size <=128) {
-      var isLowercase= comfirm ("include lowercase?")
-      console.log(isLowercase)
+      var isLowercase = confirm("include lowercase?");
+      console.log(isLowercase);
 
-      var isUppercase= confirm ("include Uppercase?")
-      console.log(isUppercase)
+      var isUppercase = confirm("include Uppercase?");
+      console.log(isUppercase);
 
-      var isNumber = confirm ("include Number?")
-      console.log(isNumber)
+      var isNumber = confirm("include Number?");
+      console.log(isNumber);
 
-      var isSpecial = confirm ("include Special?")
-      console.log(isSpecial)
+      var isSpecial = confirm("include Special?");
+      console.log(isSpecial);
 
       if (isLowercase ===true) {
+        var positionString=Math.floor(Math.random() *26);
+      
+      
+        finalpwd=finalpwd + lowercase[positionString];
+        // console.log(finalpwd);
+        return finalpwd;
+
+       for (i=0; i < size; i++){
+         
+       }
 
       }
       
@@ -48,9 +63,11 @@ function writePassword() {
       alert('invalid entery')
     }
 
+    
 
 
-}
+
+} 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
